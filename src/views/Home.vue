385a126a11de7bcd410eb1855c7a5382c26d8743
Brian <riceb53@gmail.com>
2018-11-15 20:57:14 -0600

    make vue app skeleton

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <p>{{ secondMessage }}</p>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Goodbye to Vue.js!",
+      secondMessage: "i am the second message"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
 </script>
